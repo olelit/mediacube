26f70e8e9dbd887f7c5441a649118b28269295ed
olelit <litasov00@gmail.com>
2019-02-25 10:33:22 +0300

    side ready to replace"

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
-    <link rel="stylesheet" href="{{asset('css/reset.css')}}">
+    <link rel="stylesheet" href="{{asset('/public/css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('/public/css/custom.css')}}">
+    <link rel="stylesheet" href="{{asset('/public/css/reset.css')}}">
 
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
@@ -28,7 +28,7 @@
 <footer>
     @include('partials.footer')
 </footer>
-<script src="{{asset("js/app.js")}}"></script>
-<script src="{{asset("js/custom.js")}}"></script>
+<script src="{{asset("/public/js/app.js")}}"></script>
+<script src="{{asset("/public/js/custom.js")}}"></script>
 </body>
 </html>
